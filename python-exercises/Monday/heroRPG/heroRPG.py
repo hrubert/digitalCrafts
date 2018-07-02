6006ed6c2aa52107e004ebc6679dd79d9f960660
Tracy Herbert <tracynherbert@gmail.com>
2018-07-02 11:11:52 -0500

    created goblin and hero classes

---
+++ python-exercises/Monday/heroRPG/heroRPG.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+# In this simple RPG game, the hero fights the goblin. He has the options to:
+
+# 1. fight goblin
+# 2. do nothing - in which case the goblin will attack him anyway
+# 3. flee
+
+class Hero:
+    def __init__(self):
+        self.health = 10
+        self.power = 5
+
+class Goblin:
+    def __init__(self):
+        self.health = 6
+        self.power = 2
+
+hero = Hero()
+goblin = Goblin()
+
+def main():
+    while goblin.health > 0 and hero.health > 0:
+        print("You have {} health and {} power.".format(hero.health, hero.power))
+        print("The goblin has {} health and {} power.".format(goblin.health, goblin.power))
+        print()
+        print("What do you want to do?")
+        print("1. fight goblin")
+        print("2. do nothing")
+        print("3. flee")
+        print("> ", end=' ')
+        raw_input = input()
+        if raw_input == "1":
+            # Hero attacks goblin
+            goblin.health -= hero.power
+            print("You do {} damage to the goblin.".format(hero.power))
+            if goblin.health <= 0:
+                print("The goblin is dead.")
+        elif raw_input == "2":
+            pass
+        elif raw_input == "3":
+            print("Goodbye.")
+            break
+        else:
+            print("Invalid input {}".format(raw_input))
+
+        if goblin.health > 0:
+            # Goblin attacks hero
+            hero.health -= goblin.power
+            print("The goblin does {} damage to you.".format(goblin.power))
+            if hero.health <= 0:
+                print("You are dead.")
+
+main()
