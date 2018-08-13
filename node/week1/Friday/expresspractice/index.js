b3fad33b027e59f3150b854492a7fd0ddbf22feb
Tracy Herbert <tracynherbert@gmail.com>
2018-08-13 11:25:31 -0500

    finished express exercises

--- node/week1/Friday/expresspractice/index.js
+++ node/week1/Friday/expresspractice/index.js
@@ -17,4 +17,13 @@ app.get("cats_and_dogs", function(req, res) {
     res.send("Living together")
 })
 
+app.get('/greet/:name', (req, res) => {
+    res.send(`<h1>Hello, ${req.params.name}!`)
+})
+
+app.get("/year", (req, res) => {
+    var age = req.query.age || 0;
+    res.send(`You were born in ${2018 - age}.`)
+})
+
 app.listen(3000);
