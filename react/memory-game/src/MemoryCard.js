5a25935f495ce097c9af01dad2d38cb5fd499b51
Tracy Herbert <tracynherbert@gmail.com>
2018-09-11 13:37:21 -0500

    added card flipping

--- react/memory-game/src/MemoryCard.js
+++ react/memory-game/src/MemoryCard.js
@@ -4,11 +4,32 @@ import logo from './logo.png';
 
 
 class MemoryCard extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      isFlipped: false,
+     };
+  }
+  clickHandler() {
+    this.setState({ isFlipped: !this.state.isFlipped});
+  }
+  
   render() {
+    let memoryCardInnerClass = 'MemoryCardInner';
+    if (this.state.isFlipped) {
+      memoryCardInnerClass += " flipped";
+    }
     return (
-        <div class="MemoryCard">
-            <img src={logo} />
-        </div>
+      <div class="MemoryCard" onClick={this.clickHandler.bind(this)}>
+          <div class={memoryCardInnerClass}>
+              <div class="MemoryCardBack">
+                  <img src={logo} alt=""/>
+              </div>
+              <div class="MemoryCardFront">
+                ∆
+              </div>
+          </div>
+      </div>
     );
   }
 }
